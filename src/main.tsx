94a48cfcf46d00a4213f80036c8e87bda5ed76c1
marumugam <marumugam@splunk.com>
2026-07-14 08:14:54 -0700

    Restore Vite application entry files

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,16 @@
-import React from'react';import{createRoot}from'react-dom/client';import App from'./App';import'./styles.css';createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element was not found");
+}
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
